c9bbc77063485ed3ecff7157821e4feb5aca34c0
Francisco Morais <fgmorais18@gmail.com>
2021-07-26 14:54:53 -0300

    feat: add users remove and edit

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -5,6 +5,7 @@ import { Divider } from '../../components/Divider';
 import { TitleBar } from '../../components/TitleBar';
 
 import Delete from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 import api from '../../services/api';
 
@@ -20,22 +21,38 @@ interface UserProps {
 
 export function Users() {
   const [users, setUsers] = useState<UserProps[]>([]);
+  const [user, setUser] = useState<UserProps>();
   const [modalCreateIsOpen, setModalCreateIsOpen] = useState(false);
+  const [modalEditIsOpen, setModalEditIsOpen] = useState(false);
 
+  const [userID, setUserID] = useState<number>();
   const [userName, setUserName] = useState(''); 
   const [userEmail, setUserEmail] = useState('');
   const [userCPF, setUserCPF] = useState('');
   const [userPermission, setUserPermission] = useState('');
   const [userPassword, setUserPassword] = useState('');
 
-  function openModal() {
+  function openModalCreate() {
     setModalCreateIsOpen(true);
   }
 
-  function closeModal() {
+  function closeModalCreate() {
     setModalCreateIsOpen(false);
   }
 
+  function openModalEdit() {
+    setModalEditIsOpen(true);
+  }
+
+  function closeModalEdit() {
+    setModalEditIsOpen(false);    
+    setUserID(0);
+    setUserName('');
+    setUserEmail('');
+    setUserCPF('');
+    setUserPermission('');
+    setUserPassword(''); 
+  }
 
   function getUsers() {
     api.get("/users").then((response) => setUsers(response.data));
@@ -47,15 +64,53 @@ export function Users() {
 
   async function CreateUser(event: React.FormEvent) {
     event.preventDefault();
-    closeModal();
+    closeModalCreate();
+    
+    if(userName && userEmail && userCPF && userPermission && userPassword) {
+      await api.post("/users", {      
+        name: userName, 
+        email: userEmail,
+        cpf: userCPF,
+        permission: userPermission,
+        password: userPassword,      
+      });
+    } else {
+      alert("Preencha os campos corretamente!");           
+    }
 
-    await api.post("/users", {      
-      name: userName, 
-      email: userEmail,
-      cpf: userCPF,
-      permission: userPermission,
-      password: userPassword,      
-    });
+    setUserID(0);
+    setUserName('');
+    setUserEmail('');
+    setUserCPF('');
+    setUserPermission('');
+    setUserPassword('');  
+    
+    getUsers();
+  }
+
+  async function EditUser(event: React.FormEvent) {
+    event.preventDefault();
+    closeModalEdit();
+    
+    if(userName && userEmail && userCPF && userPermission && userPassword) {
+      await api.put(`/users/${userID}`, {             
+        name: userName, 
+        email: userEmail,
+        cpf: userCPF,
+        permission: userPermission,
+        password: userPassword,      
+      });
+    } else {
+      alert("Preencha os campos corretamente!");           
+    }
+
+    setUserID(0);
+    setUserName('');
+    setUserEmail('');
+    setUserCPF('');
+    setUserPermission('');
+    setUserPassword('');  
+    
     getUsers();
   }
 
@@ -69,59 +124,135 @@ export function Users() {
     }
   }
 
+  async function HandleEditUser(id?: number) {        
+    await api.get(`/users/${id}`).then((response) => {
+      setUser(response.data);       
+      console.log(response.data);    
+    })    
+    openModalEdit();
+  }
+  
+  useEffect(() => {
+    if(user) {
+      setUserID(user.id);
+      setUserName(user.name);
+      setUserEmail(user.email);
+      setUserCPF(user.cpf);
+      setUserPermission(user.permission);
+      setUserPassword(user.password);  
+    }    
+  }, [user]);  
+
   return (
     <div className={styles.container}>
       <TitleBar title="Usuários"/>
       <Divider />
 
       <Modal
+        id="createUser"
+        contentLabel="createUser"
         isOpen={modalCreateIsOpen}        
-        onRequestClose={closeModal}                 
+        onRequestClose={closeModalCreate}   
+        ariaHideApp={false}              
       >
-          <h2 style={{ textAlign: 'center' }}>Criar usuário</h2>        
-          <div className={styles.modal}>
-            <form onSubmit={(e) => CreateUser(e)}>
-              <p>Nome:</p>
-              <input 
-                type="text" 
-                onChange={e => setUserName(e.target.value)}
-              />
-              
-              <p>Email:</p>
-              <input 
-                type="email"
-                onChange={e => setUserEmail(e.target.value)}
-              />
+        <h2 style={{ textAlign: 'center' }}>Criar usuário</h2>        
+        <div className={styles.modal}>
+          <form onSubmit={(e) => CreateUser(e)}>
+            <p>Nome:</p>
+            <input 
+              type="text" 
+              onChange={e => setUserName(e.target.value)}
+            />
+            
+            <p>Email:</p>
+            <input 
+              type="email"
+              onChange={e => setUserEmail(e.target.value)}
+            />
 
-              <p>CPF:</p>      
-              <input 
-                type="text"
-                onChange={e => setUserCPF(e.target.value)}
-              />          
+            <p>CPF:</p>      
+            <input 
+              type="text"
+              onChange={e => setUserCPF(e.target.value)}
+            />          
 
-              <p>Permissão:</p>      
-              <input 
-                type="text"
-                onChange={e => setUserPermission(e.target.value)}
-              />         
+            <p>Permissão:</p>      
+            <input 
+              type="text"
+              onChange={e => setUserPermission(e.target.value)}
+            />         
 
-              <p>Senha:</p>      
-              <input 
-                type="password"
-                onChange={e => setUserPassword(e.target.value)}
-              />                  
+            <p>Senha:</p>      
+            <input 
+              type="password"
+              onChange={e => setUserPassword(e.target.value)}
+            />                  
 
-              <div>
-                <button type="submit" onClick={CreateUser}>Criar</button>              
-              </div>
-            </form>
-          </div>
-          <button onClick={closeModal}>close</button>
+            <div className={styles.formActionButton}>
+              <button onClick={closeModalCreate}>Fechar</button>
+              <button type="submit" onClick={CreateUser}>Criar</button>              
+            </div>
+          </form>
+        </div>
+      </Modal>
+
+      <Modal
+        id="editUser"
+        contentLabel="editUser"
+        isOpen={modalEditIsOpen}        
+        onRequestClose={closeModalEdit}                 
+        ariaHideApp={false}
+      >
+        <h2 style={{ textAlign: 'center' }}>Criar usuário</h2>        
+        <div className={styles.modal}>
+          <form onSubmit={(e) => EditUser(e)}>
+            <p>Nome:</p>
+            <input 
+              type="text" 
+              value={userName}              
+              onChange={e => setUserName(e.target.value)}
+            />
+            
+            <p>Email:</p>
+            <input 
+              type="email"
+              value={userEmail}
+              onChange={e => setUserEmail(e.target.value)}
+            />
+
+            <p>CPF:</p>      
+            <input 
+              type="text"
+              value={userCPF}
+              onChange={e => setUserCPF(e.target.value)}
+            />          
+
+            <p>Permissão:</p>      
+            <input 
+              type="text"
+              value={userPermission}
+              onChange={e => setUserPermission(e.target.value)}
+            />         
+
+            <p>Senha:</p>      
+            <input 
+              type="password"
+              value={userPassword}
+              disabled
+              onChange={e => setUserPassword(e.target.value)}
+            />                  
+
+            <div className={styles.formActionButton}>
+              <button onClick={closeModalEdit}>Fechar</button>
+              <button type="submit" onClick={EditUser}>Criar</button>              
+            </div>
+          </form>
+        </div>
       </Modal>
 
       <div>
         <div className={styles.header}>
-          <button className={styles.addUser} onClick={openModal}>
+          <button className={styles.addUser} onClick={openModalCreate}>
             Adicionar usuário
           </button>
         </div>
@@ -138,15 +269,17 @@ export function Users() {
           <tbody>
             {users?.map((user) => {
               return (
-                <tr>
+                <tr key={user.id}>
                   <td>{user.id}</td>
                   <td>{user.name}</td>    
                   <td>{user.email}</td> 
                   <td>{user.cpf}</td>                  
                   <td>{user.permission}</td> 
-                  <td>                    
-                    <button onClick={() => DeleteUser(user.id)}>Editar</ button>
-                    <button onClick={() => DeleteUser(user.id)}>
+                  <td style={{ width: '150px' }}>                    
+                    <button className={styles.tableActionButton} onClick={() => HandleEditUser(user.id)}>
+                      <EditIcon />
+                    </button>
+                    <button className={styles.tableActionButton} onClick={() => DeleteUser(user.id)}>
                       <Delete />
                     </button>
                   </td>                       
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,22 @@
-import { Switch, Route, BrowserRouter, Link, NavLink } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, NavLink } from 'react-router-dom';
 
 import { Dashboard } from './pages/Dashboard';
 import { Home } from './pages/Home';
 import { Users } from './pages/Users';
+import { Cars } from './pages/Cars';
 
 const pages = [
   {
     name: 'Dashboard',
-    address: '/dashboard'
+    address: '/dashboard',    
   },
   {
-    name: 'Users',
-    address: '/users'
+    name: 'Usuários',
+    address: '/users',    
+  },
+  {
+    name: 'Carros',
+    address: '/cars',    
   },
 ]
 
@@ -20,15 +25,16 @@ export default function AppRouter() {
     <BrowserRouter>
       <div className="sidebar">
         <div className="main-logo">
-          <Link to="/"><img src="/xicodes-logo.png" alt="" /></Link>        
+          <NavLink to="/"><img src="/localiza-logo.png" alt="" /></NavLink>        
         </div>
         <div className="menu-container">
           <nav>
-            {pages.map((page) => {
+            {pages.map((page, index) => {
               return (
-                <div className="nav-button">
+                <div className="nav-button" key={index}>
                   <NavLink 
-                    exact to={page.address}                    
+                    exact 
+                    to={page.address}                    
                   >
                     {page.name}
                   </NavLink>
@@ -43,12 +49,15 @@ export default function AppRouter() {
           <Switch>            
             <Route exact path="/">
               <Home />
-            </Route>
+            </Route>            
             <Route path="/dashboard">
               <Dashboard />
             </Route>
             <Route path="/users">
               <Users />
+            </Route>
+            <Route path="/cars">
+              <Cars />
             </Route>
           </Switch>
         </Route>
