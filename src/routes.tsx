06e7675b1fb18c8cef1afd44f57bf0f2259811d3
Francisco Morais <fgmorais18@gmail.com>
2021-07-26 11:30:06 -0300

    feat: add sidebar styles

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,34 +1,40 @@
-import { Switch, Route, BrowserRouter, Link } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, Link, NavLink } from 'react-router-dom';
 
 import { Dashboard } from './pages/Dashboard';
 import { Home } from './pages/Home';
+import { Users } from './pages/Users';
+
+const pages = [
+  {
+    name: 'Dashboard',
+    address: '/dashboard'
+  },
+  {
+    name: 'Users',
+    address: '/users'
+  },
+]
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <div className="sidebar">
         <div className="main-logo">
-          <Link to="/" style={{ textDecoration: 'none' }}><h1>LOGO</h1></Link>        
+          <Link to="/"><img src="/xicodes-logo.png" alt="" /></Link>        
         </div>
         <div className="menu-container">
           <nav>
-            <div className="nav-button">
-              <Link to="/dashboard">Dashboard</Link>
-            </div>
-            <div className="nav-button">
-              <Link to="/users">
-                <span>Usuários</span>
-              </Link>
-            </div>
-            <div>
-              <Link to="/cars"><span>Carros</span></Link>
-            </div>
-            <div>
-              <Link to="/rent"><span>Aluguel</span></Link>
-            </div>
-            <div>
-              <Link to="/settings"><span>Configurações</span></Link>
-            </div>
+            {pages.map((page) => {
+              return (
+                <div className="nav-button">
+                  <NavLink 
+                    exact to={page.address}                    
+                  >
+                    {page.name}
+                  </NavLink>
+                </div>
+              );
+            })}
           </nav>
         </div>
       </div>
@@ -40,6 +46,9 @@ export default function AppRouter() {
             </Route>
             <Route path="/dashboard">
               <Dashboard />
+            </Route>
+            <Route path="/users">
+              <Users />
             </Route>
           </Switch>
         </Route>
