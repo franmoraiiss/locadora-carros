f080962c3bb5b65511312af34c4be6d96c3d4687
Francisco Morais <fgmorais18@gmail.com>
2021-08-02 19:01:02 -0300

    styles: add home styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import styles from './styles.module.scss';
+
 export function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Bem-vindo ao sistema Localiza.</h1>
-      <p>Use a barra de sistema navegação para navegar entre as funcionalidades</p>
-      <p>xicodes - Nenhum direito reservado - 2021</p>
+      
+      <div>
+        <p>Use a barra de sistema navegação para navegar entre as funcionalidades</p>      
+        <p>xicodes - Todos os direitos reservados - 2021</p>
+      </div>
     </div>
   )
 }
